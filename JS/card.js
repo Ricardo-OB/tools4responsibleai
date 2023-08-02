fe6bea9eecdb7cff62d7b49c6b86e1c473bee3f1
Ricardo <ricardorob587@gmail.com>
2023-08-01 21:49:28 -0500

    Updates

--- JS/card.js
+++ JS/card.js
@@ -10,13 +10,17 @@ async function showCardTool(id) {
     let [levelDevCardA, levelDevCardB, levelDevCardC, levelDevCardD] = [document.getElementById("card-level-dev-A"), document.getElementById("card-level-dev-B"), document.getElementById("card-level-dev-C"), document.getElementById("card-level-dev-D")];
     let [urlCardA, urlCardB, urlCardC, urlCardD] = [document.getElementById("card-url-A"), document.getElementById("card-url-B"), document.getElementById("card-url-C"), document.getElementById("card-url-D")];
 
-    let original = 'Card -';
+    let component_tool = document.getElementById(id);
+    let num_tool = component_tool.innerText;
 
-    component_tool = document.getElementById(id);
-    num_tool = component_tool.innerText;
+    let infoTool = getInfoTool(num_tool);
 
-    let values = getInfoTool(num_tool);
-    let enlacesJoint = (await values).links;
+    let idTool = (await infoTool).id, name = (await infoTool).name, otherName = (await infoTool).otherName, description = (await infoTool).description, stage = (await infoTool).stage;
+    let highPrinciple = (await infoTool).highPrinciple, lowPrinciple = (await infoTool).lowPrinciple, task = (await infoTool).task, levelDev = (await infoTool).levelDev;
+    let sector = (await infoTool).sector, toolType = (await infoTool).toolType, language = (await infoTool).language, urldoi = (await infoTool).urlDoi;
+    let year = (await infoTool).year, authors = (await infoTool).authors, links = (await infoTool).links;
+
+    let enlacesJoint = (await infoTool).links;
     let enlaces = enlacesJoint.split("\n\n"); // array con enlaces
     
     for (var key in jsonCards) {
@@ -28,37 +32,37 @@ async function showCardTool(id) {
                 console.log("none");
                 element.style.display = 'flex';
                 if (key == 'cardA') {
-                    featureCardA.innerText = "Tool " + id;
-                    titleCardA.innerText = (await values).nameT;
-                    sectorCardA.innerText = (await values).sector;
-                    taskCardA.innerText = (await values).task;
-                    levelDevCardA.innerText = (await values).levelDev;
-                    urlCardA.innerText = (await values).url;
-                    urlCardA.href = (await values).url;
+                    featureCardA.innerText = "Tool " + idTool;
+                    titleCardA.innerText = (await infoTool).nameT;
+                    sectorCardA.innerText = (await infoTool).sector;
+                    taskCardA.innerText = (await infoTool).task;
+                    levelDevCardA.innerText = (await infoTool).levelDev;
+                    urlCardA.innerText = links;
+                    urlCardA.href = (await infoTool).url;
                 } else if (key == 'cardB') {
                     featureCardB.innerText = "Tool " + id;
-                    titleCardB.innerText = (await values).nameT;
-                    sectorCardB.innerText = (await values).sector;
-                    taskCardB.innerText = (await values).task;
-                    levelDevCardB.innerText = (await values).levelDev;
-                    urlCardB.innerText = (await values).url;
-                    urlCardB.href = (await values).url;
+                    titleCardB.innerText = (await infoTool).nameT;
+                    sectorCardB.innerText = (await infoTool).sector;
+                    taskCardB.innerText = (await infoTool).task;
+                    levelDevCardB.innerText = (await infoTool).levelDev;
+                    urlCardB.innerText = (await infoTool).url;
+                    urlCardB.href = (await infoTool).url;
                 } else if (key == 'cardC') {
                     featureCardC.innerText = "Tool " + id;
-                    titleCardC.innerText = (await values).nameT;
-                    sectorCardC.innerText = (await values).sector;
-                    taskCardC.innerText = (await values).task;
-                    levelDevCardC.innerText = (await values).levelDev;
-                    urlCardC.innerText = (await values).url;
-                    urlCardC.href = (await values).url;
+                    titleCardC.innerText = (await infoTool).nameT;
+                    sectorCardC.innerText = (await infoTool).sector;
+                    taskCardC.innerText = (await infoTool).task;
+                    levelDevCardC.innerText = (await infoTool).levelDev;
+                    urlCardC.innerText = (await infoTool).url;
+                    urlCardC.href = (await infoTool).url;
                 } else if (key == 'cardD') {
                     featureCardD.innerText = "Tool " + id;
-                    titleCardD.innerText = (await values).nameT;
-                    sectorCardD.innerText = (await values).sector;
-                    taskCardD.innerText = (await values).task;
-                    levelDevCardD.innerText = (await values).levelDev;
-                    urlCardD.innerText = (await values).url;
-                    urlCardD.href = (await values).url;
+                    titleCardD.innerText = (await infoTool).nameT;
+                    sectorCardD.innerText = (await infoTool).sector;
+                    taskCardD.innerText = (await infoTool).task;
+                    levelDevCardD.innerText = (await infoTool).levelDev;
+                    urlCardD.innerText = (await infoTool).url;
+                    urlCardD.href = (await infoTool).url;
                 }
                 break;
             }
@@ -70,18 +74,18 @@ async function showCardTool(id) {
     // } else {
     //     cardA.style.display = 'flex';
     //     featureCardA.innerText = "Tool " + id;
-    //     titleCardA.innerText = (await values).nameT;
-    //     sectorCardA.innerText = (await values).sector;
-    //     taskCardA.innerText = (await values).task;
-    //     levelDevCardA.innerText = (await values).levelDev;
-    //     urlCardA.innerText = (await values).url;
-    //     urlCardA.href = (await values).url;
+    //     titleCardA.innerText = (await infoTool).nameT;
+    //     sectorCardA.innerText = (await infoTool).sector;
+    //     taskCardA.innerText = (await infoTool).task;
+    //     levelDevCardA.innerText = (await infoTool).levelDev;
+    //     urlCardA.innerText = (await infoTool).url;
+    //     urlCardA.href = (await infoTool).url;
     // }
 }
 
 async function getInfoTool(id) {
-    let Name = "", Task = "", Sector = "", LevelDev = "", URL = "", Year = "", Authors = "", Links = "";
-    url = "https://raw.githubusercontent.com/Ricardo-OB/tools-ethical-dev-ai/master/csv/herramientas_prueba.csv";
+
+    const url = 'https://raw.githubusercontent.com/Ricardo-OB/tools-ethical-dev-ai/master/csv/final_resources_v2.csv';
     const options = {
         url,
         responseType: "arraybuffer"
@@ -92,26 +96,40 @@ async function getInfoTool(id) {
     let firstSheetName = workbook.SheetNames[0];
     let jsonDataRaw = XLSX.utils.sheet_to_json(workbook.Sheets[firstSheetName]);
 
-    console.log(jsonDataRaw);
-    console.log(jsonDataRaw.length);
+    // console.log(jsonDataRaw);
+    // console.log(jsonDataRaw.length);
 
+    let Id = "", Name = "", OtherName = "", Description = "", Stage = "", HighPrinciple = "", LowPrinciple = "", Task = "", LevelDev = "", Sector = "", ToolType = "", Language = "", URLDOI = "", Year = "", Authors = "", Links = "";
+    
     for (let i=0; i<jsonDataRaw.length; i++){
         let row = jsonDataRaw[i];
-        if (row["ID"] == id) {
-          console.log("ID encontrado: " + row["ID"]);
-          Name = row["NAME"];
-          Task = row["TASK"];
-          Sector = row["SECTOR"];
-          LevelDev = row["LEVEL_DEV"];
-          URL = row["DOI/URL"];
-          Year = row["YEAR"];
-          Authors = row["AUTHOR(S)"];
-          Links = row["OTHER_LINKS"];
-          break;
+            if (row["ID"] == id) {
+            console.log("ID encontrado: " + row["ID"]);
+            /*
+            ID, Name, Article / Other Name, Description, AI Life Cycle Stage, High Level Principle, Associated Principles, Task, Level of Development, Sector, Tool Type, Programming Language, DOI / URL, Year, Authors / Owners, Other Links 
+            */
+            Id = row["ID"];
+            Name = row["Name"];
+            OtherName = row["Article / Other Name"];
+            Description = row["Description"];
+            Stage = row["AI Life Cycle Stage"];
+            HighPrinciple = row["High Level Principle"];
+            LowPrinciple = row["Associated Principles"];
+            Task = row["Task"];
+            LevelDev = row["Level of Development"];
+            Sector = row["sector"];
+            ToolType = row["Tool Type"];
+            Language = row["Programming Language"];
+            URLDOI = row["DOI / URL"];
+            Year = row["Year"];
+            Authors = row["Authors / Owners"];
+            Links = row["Other Links"];
+            break;
         }
     }
 
-    return {nameT: Name, task: Task, sector: Sector, levelDev: LevelDev, url: URL, year: Year, autohrs: Authors, links: Links};
+    return {id: Id, name: Name, otherName: OtherName, description: Description, stage: Stage, highPrinciple: HighPrinciple, lowPrinciple: LowPrinciple,
+            task: Task, levelDev: LevelDev, sector: Sector, toolType: ToolType, language: Language, urlDoi: URLDOI, year: Year, authors: Authors, links: Links};
 }
 
 async function cleanCardA(){
